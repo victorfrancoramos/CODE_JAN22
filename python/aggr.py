99a132aabb174265513e2e8e234e57c02cf0e32e
victorfrancoramos <formeitor@gmail.com>
2022-01-19 07:07:00 +0100

    Changed code to aggr

--- python/aggr.py
+++ python/aggr.py
@@ -5,7 +5,7 @@ from netapp_ontap.resources import Aggregate
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-conn = HostConnection("192.168.0.101", username = "admin", password = "Netapp1!", verify = False)
+conn = HostConnection("192.168.0.102", username = "admin", password = "Netapp1!", verify = False)
 config.CONNECTION = conn
 aggr = Aggregate()
 aggr.get()
